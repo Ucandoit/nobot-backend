5191047c09ab7b9e362a0a922b2295bcf6af96fa
Ucandoit <yukan.zhao@gmail.com>
2020-04-13 11:13:23 +0200

    feature: add search method to card repository

--- src/main/java/io/ucandoit/nobot/repository/CardRepository.java
+++ src/main/java/io/ucandoit/nobot/repository/CardRepository.java
@@ -1,8 +1,12 @@
 package io.ucandoit.nobot.repository;
 
 import io.ucandoit.nobot.model.Card;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface CardRepository extends JpaRepository<Card, Integer> {}
+public interface CardRepository extends JpaRepository<Card, Integer> {
+  Page<Card> findCardsByRarity(String rarity, Pageable pageable);
+}
